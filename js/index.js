24d712a84b6c197a2e93803c466dc096e08d9939
DarkSemiDeus <137968477+DarkSemiDeus@users.noreply.github.com>
2024-03-26 03:19:38 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.getElementById("declaracaoButton").addEventListener("click", function() {
+    window.location.href = "declaracao.html";
+});
+
+document.getElementById("ajudaButton").addEventListener("click", function() {
+    window.location.href = "ajuda.html";
+});
+
+document.getElementById("feedbackButton").addEventListener("click", function() {
+    window.location.href = "feedback.html";
+});
